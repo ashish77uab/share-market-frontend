2b50b2b17d1e735c7156fd3423cf655867aa1ee8
Ashish Patel <apatel@grepruby.io>
2024-11-21 21:41:57 +0530

    fix: wishlist

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -3,18 +3,21 @@ import React, { useEffect, useState } from "react";
 import ToastMsg from "../components/toast/ToastMsg";
 import { toast } from "react-toastify";
 import axios from "axios";
+import moment from 'moment';
 // import { NseIndia } from  "stock-nse-india";
 const Wishlist = () => {
 
   const [fetchLoading, setFetchLoading] = useState(false)
 
+
   const getMatchDetail = async (id) => {
     setFetchLoading(true)
     try {
-      const res = await axios.get(`https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=3WPMKT6OXFBBVV4T`);
+      const res = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=NG&interval=5min&apikey=3WPMKT6OXFBBVV4T`);
       const { status, data } = res;
       if (status >= 200 && status <= 300) {
         console.log(data);
+        // setIntraData(data)
       } else {
         toast.error(<ToastMsg title={data.message} />);
       }
@@ -25,52 +28,51 @@ const Wishlist = () => {
     }
   };
   useEffect(() => {
+    // getMatchDetail()
     var data = JSON.stringify({
       "clientcode": "M140361",
       "password": "9331",
-      "totp": "zM1oeDjN"
+      "totp": "QZVW7W3OOBL2F2EQ3MPSMGGROQ",
+    //  "state":"state_or_environment_variable"
     });
 
     var config = {
       method: 'post',
-      url: 'https://apiconnect.angelbroking.com/rest/auth/angelbroking/user/v1/loginByPassword',
-      headers: {
+      url: 'https://apiconnect.angelone.in/rest/auth/angelbroking/user/v1/loginByPassword',
+
+  headers : {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'X-UserType': 'USER',
         'X-SourceID': 'WEB',
         'X-ClientLocalIP': '192.168.1.11',
-        'X-ClientPublicIP': '106.219.85.203',
-        'X-PrivateKey': 'My Private Key'
-      },
-      data: data
-    };
+        // 'X-ClientPublicIP': '192.168.1.11',
+        'X-PrivateKey': 'zM1oeDjN'
+  },
+    data: data
+  };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}, [])
 
-    // getMatchDetail()
-  }, [])
-
-  return (
-    <>
-      <section className="py-24 container">
-        <div className="flex gap-10 items-start">
-          <div className="flex-1">
-
-          </div>
+return (
+  <div className="bg-sky-50 py-10">
+    <section className=" container">
+      <h3 className="heading-3 mb-2">My Wishlist</h3>
+      <div>
 
 
-        </div>
-      </section>
+      </div>
+    </section>
 
-    </>
-  );
+  </div>
+);
 };
 
 export default Wishlist
