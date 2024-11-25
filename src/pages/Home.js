088beb8366d373f5f26bd6d212e29c12444307c3
Ashish Patel <apatel@grepruby.io>
2024-11-25 11:31:29 +0530

    feat: strategies page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -113,7 +113,7 @@ const Home = () => {
           </div>
 
         </div>
-        <div className="py-20  ">
+        {/* <div className="py-20  ">
           <div className="container">
             <header>
               <div className="container flex justify-center mb-10 lg:mb-16  items-center">
@@ -129,7 +129,7 @@ const Home = () => {
             </div>
           </div>
 
-        </div>
+        </div> */}
 
         <div className="py-20  ">
           <div className="container">
